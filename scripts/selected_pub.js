42091686d03d4dc362025e8c7b9dc3f46d238062
cswxia <49634306+cswxia@users.noreply.github.com>
2023-10-22 10:33:52 +0800

    Update selected_pub.js

--- scripts/selected_pub.js
+++ scripts/selected_pub.js
@@ -1,94 +1,94 @@
 pubs = [
      {
-        "title": "Light-Dedup: A Light-weight Inline Deduplication Framework for Non-Volatile Memory File Systems",
-        "authors": "Jiansheng Qiu, Yanqi Pan, Wen Xia, Xiaojia Huang, Wenjun Wu, Xiangyu Zou, Shiyi Li, and Yu Hua",
+        "title": "Light-Dedup: A Light-weight Inline Deduplication Framework for Non-Volatile Memory File Systems.",
+        "authors": "Jiansheng Qiu, Yanqi Pan, Wen Xia, Xiaojia Huang, Wenjun Wu, Xiangyu Zou, Shiyi Li, and Yu Hua.",
         "href": "https://www.usenix.org/conference/atc23/",
         "epilogue": "Proceedings of the 2022 USENIX Annual Technical Conference  (USENIX ATC'23), Carlsbad, CA, USA, Jul. 2023. (Acceptance rate: 18%)"
     },
     {
-        "title": "HUNTER: Releasing Persistent Memory Write Performance with A Novel PM-DRAM Collaboration Architecture",
-        "authors": "Yanqi Pan, Yifeng Zhang, Wen Xia, Xiangyu Zou, Cai Deng",
+        "title": "HUNTER: Releasing Persistent Memory Write Performance with A Novel PM-DRAM Collaboration Architecture.",
+        "authors": "Yanqi Pan, Yifeng Zhang, Wen Xia, Xiangyu Zou, Cai Deng.",
         "href": "10.1109/DAC56929.2023.10247940",
-        "epilogue": "Proceedings of 2023 60th ACM/IEEE Design Automation Conference (DAC), San Francisco, CA, USA, 2023, pp. 1-6"
+        "epilogue": "Proceedings of 2023 60th ACM/IEEE Design Automation Conference (DAC), San Francisco, CA, USA, 2023."
     },
     {
-        "title": "EEPH: An Efficient Extendible Perfect Hashing for Hybrid PMem-DRAM",
-        "authors": "Qi Chen, Hao Hu, Cai Deng, Dingbang Liu, Shiyi Li, Bo Tang, Ting Yao, and Wen Xia",
+        "title": "EEPH: An Efficient Extendible Perfect Hashing for Hybrid PMem-DRAM.",
+        "authors": "Qi Chen, Hao Hu, Cai Deng, Dingbang Liu, Shiyi Li, Bo Tang, Ting Yao, and Wen Xia.",
         "href": "10.1109/ICDE55515.2023.00109",
-        "epilogue": "Proceedings of 2023 IEEE 39th International Conference on Data Engineering (ICDE). IEEE, 2023"
+        "epilogue": "Proceedings of 2023 IEEE 39th International Conference on Data Engineering (ICDE). IEEE, 2023."
     },
    
     {
-        "title": "Building a High-performance Fine-grained Deduplication Framework for Backup Storage with High Deduplication Ratio",
-        "authors": "Xiangyu Zou, Wen Xia, Philip Shilane, Haijun Zhang, and Xuan Wang",
+        "title": "Building a High-performance Fine-grained Deduplication Framework for Backup Storage with High Deduplication Ratio.",
+        "authors": "Xiangyu Zou, Wen Xia, Philip Shilane, Haijun Zhang, and Xuan Wang.",
         "href": "https://www.usenix.org/conference/atc22/",
         "epilogue": "Proceedings of the 2022 USENIX Annual Technical Conference (USENIX ATC'22), Carlsbad, CA, USA, Jul. 2022. (Acceptance rate: 16%)"
     },
     
     {
-        "title": "imDedup: A Lossless Deduplication Scheme to Eliminate Fine-grained Redundancy among Images",
-        "authors": "Cai Deng, Qi Chen, Xiangyu Zou, Erci Xu, Bo Tang, and Wen Xia",
+        "title": "imDedup: A Lossless Deduplication Scheme to Eliminate Fine-grained Redundancy among Images.",
+        "authors": "Cai Deng, Qi Chen, Xiangyu Zou, Erci Xu, Bo Tang, and Wen Xia.",
         "href": "10.1109/ICDE53745.2022.00085",
-        "epilogue": "Proceedings of 2022 IEEE 38th International Conference on Data Engineering (ICDE), Kuala Lumpur, Malaysia, 2022, pp. 1071-1084."
+        "epilogue": "Proceedings of 2022 IEEE 38th International Conference on Data Engineering (ICDE), Kuala Lumpur, Malaysia, 2022."
     },
     {
         "title": "The Dilemma between Deduplication and Locality: Can Both be Achieved?",
-        "authors": "Xiangyu Zou, Jingsong Yuan, Philip Shilane, Wen Xia, Haijun Zhang, and Xuan Wang",
+        "authors": "Xiangyu Zou, Jingsong Yuan, Philip Shilane, Wen Xia, Haijun Zhang, and Xuan Wang.",
         "href": "https://www.usenix.org/conference/fast21/presentation/zou",
         "epilogue": "Proceedings of the 19th USENIX Conference on File and Storage Technologies (FAST'21), Santa Clara, CA, USA, Feb. 2021. (Acceptance rate: 21.5%)"
     },
     {
-        "title": "Austere Flash Caching with Deduplication and Compression",
-        "authors": "Qiuping Wang, Jinhong Li, Wen Xia, Erik Kruus, Biplob Debnath, and Patrick P. C. Lee",
+        "title": "Austere Flash Caching with Deduplication and Compression.",
+        "authors": "Qiuping Wang, Jinhong Li, Wen Xia, Erik Kruus, Biplob Debnath, and Patrick P. C. Lee.",
         "href": "https://www.usenix.org/conference/atc20/presentation/wang-qiuping",
         "epilogue": "Proceedings of the 2020 USENIX Annual Technical Conference (USENIX ATC'20), Boston, MA, USA, Jul. 2020. (Acceptance rate: 17%)"
     },
     {
-        "title": "Dsync: a Lightweight Delta Synchronization Approach for Cloud Storage Services",
-        "authors": "Yuan He, Lingfeng Xiang, Wen Xia, Hong Jiang, Zhenhua Li, Xuan Wang, and Xiangyu Zou",
+        "title": "Dsync: a Lightweight Delta Synchronization Approach for Cloud Storage Services.",
+        "authors": "Yuan He, Lingfeng Xiang, Wen Xia, Hong Jiang, Zhenhua Li, Xuan Wang, and Xiangyu Zou.",
         "href": "https://storageconference.us/2020/Papers/08.DSync.pdf",
         "epilogue": "Proceedings of the 36th International Conference on Massive Storage Systems and Technology (MSST'20), Santa Clara, CA, USA, Oct. 2020."
     },
     {
-        "title": "Finesse: Fine-Grained Feature Locality based Fast Resemblance Detection for Post-Deduplication Delta Compression",
-        "authors": "Yucheng Zhang, Wen Xia, Dan Feng, Hong Jiang, Yu Hua, Qiang Wang",
+        "title": "Finesse: Fine-Grained Feature Locality based Fast Resemblance Detection for Post-Deduplication Delta Compression.",
+        "authors": "Yucheng Zhang, Wen Xia, Dan Feng, Hong Jiang, Yu Hua, and Qiang Wang.",
         "href": "https://www.usenix.org/conference/fast19/presentation/zhang",
         "epilogue": "Proceedings of the 17th USENIX Conference on File and Storage Technologies (FAST'19), BOSTON, MA, USA, Feb. 2019. (Acceptance rate: 18%)"
     },
    
     {
-        "title": "A Comprehensive Study of the Past, Present, and Future of Data Deduplication",
-        "authors": "Wen Xia, Yukun Zhou, Hong Jiang, Dan Feng, Yu Hua, Yuchong Hu, Yucheng Zhang, Qing Liu",
+        "title": "A Comprehensive Study of the Past, Present, and Future of Data Deduplication.",
+        "authors": "Wen Xia, Hong Jiang, Dan Feng, Fred Douglis, Philip Shilane, Yu Hua, Min Fu, Yucheng Zhang, and Yukun Zhou.",
         "href": "survey-final-2016.pdf",
         "epilogue": "in Proceedings of the IEEE (PIEEE), Vol. 104, No. 9, Sep. 2016, pages: 1681-1710."
     },
     {
-        "title": "FastCDC: a Fast and Efficient Content-Defined Chunking Approach for Data Deduplication",
-        "authors": "Wen Xia, Yukun Zhou, Hong Jiang, Dan Feng, Yu Hua, Yuchong Hu, Yucheng Zhang, Qing Liu",
+        "title": "FastCDC: a Fast and Efficient Content-Defined Chunking Approach for Data Deduplication.",
+        "authors": "Wen Xia, Yukun Zhou, Hong Jiang, Dan Feng, Yu Hua, Yuchong Hu, Yucheng Zhang, and Qing Liu.",
         "href": "https://www.usenix.org/system/files/conference/atc16/atc16-paper-xia.pdf",
         "epilogue": "in Proceedings of USENIX Annual Technical Conference (USENIX ATC'16), Denver, CO, USA, June 22–24, 2016, pages: 101-114. (Acceptance rate:18%, the FastCDC source code is <a href=\"https://github.com/wxiacode/FastCDC-c\">here</a>)"
     },
     {
-        "title": "Design Tradeoffs for Data Deduplication Performance in Backup Workloads",
-        "authors": "Min Fu, Dan Feng, Yu Hua, Xubin He, Zuoning Chen, Wen Xia, Yucheng Zhang, and Yujuan Tan",
+        "title": "Design Tradeoffs for Data Deduplication Performance in Backup Workloads.",
+        "authors": "Min Fu, Dan Feng, Yu Hua, Xubin He, Zuoning Chen, Wen Xia, Yucheng Zhang, and Yujuan Tan.",
         "href": "https://www.usenix.org/system/files/conference/fast15/fast15-paper-fu.pdf",
         "epilogue": "in Proceedings of the 13th USENIX Conference on File and Storage Technologies (FAST'15), Santa Clara, CA, USA, February 16-19, 2015. USENIX Association, pages: 331-344. (Acceptance rate: 21%)"
     },
     {
-        "title": "AE: An Asymmetric Extremum Content Defined Chunking Algorithm for Fast and Bandwidth-Efficient Data Deduplication",
-        "authors": "Yucheng Zhang, Hong Jiang, Dan Feng, Wen Xia, Min Fu, Fangting Huang, and Yukun Zhou",
+        "title": "AE: An Asymmetric Extremum Content Defined Chunking Algorithm for Fast and Bandwidth-Efficient Data Deduplication.",
+        "authors": "Yucheng Zhang, Hong Jiang, Dan Feng, Wen Xia, Min Fu, Fangting Huang, and Yukun Zhou.",
         "href": "pub/AE-INFOCOM-zhang-2015.pdf",
         "epilogue": "in Proceedings of the 34th IEEE International Conference on Computer Communications (INFOCOM'15), Hong Kong, China, April 26th - May 1st, 2015, pages: 1337-1345.(Acceptance rate: 19%)"
     },
     {
-        "title": "Accelerating Restore and Garbage Collection in Deduplication-based Backup Systems via Exploiting Historical Information",
-        "authors": "Min Fu, Dan Feng, Yu Hua, Xubin He, Zuoning Chen, Wen Xia, Fangting Huang, and Qing Liu",
+        "title": "Accelerating Restore and Garbage Collection in Deduplication-based Backup Systems via Exploiting Historical Information.",
+        "authors": "Min Fu, Dan Feng, Yu Hua, Xubin He, Zuoning Chen, Wen Xia, Fangting Huang, and Qing Liu.",
         "href": "https://www.usenix.org/conference/atc14/technical-sessions/presentation/fu_min",
         "epilogue": "in Proceedings of 2014 USENIX Annual Technical Conference (USENIX ATC'14), Philadelphia, PA, USA: USENIX Association, June 19-20, 2014, pages 181-192. (Acceptance rate: 15%)"
     },
     {
-        "title": "SiLo: A Similarity-Locality based Near-Exact Deduplication Scheme with Low RAM Overhead and High Throughput",
-        "authors": "Wen Xia, Hong Jiang, Dan Feng, and Yu Hua",
+        "title": "SiLo: A Similarity-Locality based Near-Exact Deduplication Scheme with Low RAM Overhead and High Throughput.",
+        "authors": "Wen Xia, Hong Jiang, Dan Feng, and Yu Hua.",
         "href": "http://static.usenix.org/events/atc11/tech/final_files/Xia.pdf",
         "epilogue": "in Proceedings of The 2011 USENIX Annual Technical Conference (USENIX ATC'11), Portland, OR, USA, June 15-17, 2011, pages 285-298. (Acceptance rate: 15%)"
     }
